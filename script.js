10d1d092b0210f16621b2ce7a178446436b69265
Jasmine East <jasmineeast@Jasmines-Air.attlocal.net>
2020-09-27 14:14:32 -0400

    added var for passwordlenth and all entries

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowerCase = "abcdefghijklmnopqrstuvwxyz".split("");
+var numeric = "0123456789".split("");
+var specialCharacter = "!@#$%^&*?+".split("");
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -5,33 +10,69 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
- // document.querySelector("#password").value=password
+  // document.querySelector("#password").value=password
 
   passwordText.value = password;
-
 }
-function generatePassword(){
-    var passwordlength=prompt("password size?")
-    console.log(passwordlength)
 
-    if(passwordlength >=8 && passwordlength <=128){
-           
-      var isLowercase= confirm("is this lowecase?")
-           console.log (isLowercase)
-      var isUppercase= confirm("is this uppercase?")
-          console.log (isUppercase)
-      var isNumeric= confirm("is this numeric?")
-          console.log (isNumeric)
-      var isSpecialCharacter= confirm("is this a special character?")
-          console.log (isSpecialCharacter)
-    } 
-    else{
-           alert("invalid password length")
+function generatePassword() {
+  var password = "";
+  var passwordlength = parseInt(prompt("password size?"));
+  console.log(passwordlength);
+
+  if (passwordlength >= 8 && passwordlength <= 128) {
+    var isLowerCase = confirm("is this lowecase?");
+    console.log(isLowerCase);
+    var isUpperCase = confirm("is this uppercase?");
+    console.log(isUpperCase);
+    var isNumeric = confirm("is this numeric?");
+    console.log(isNumeric);
+    var isSpecialCharacter = confirm("is this a special character?");
+    console.log(isSpecialCharacter);
+
+    if (isLowerCase) {
+      password =
+        password + lowerCase[Math.floor(Math.random() * lowerCase.length)];
     }
+    if (isUpperCase) {
+      password =
+        password + upperCase[Math.floor(Math.random() * upperCase.length)];
+    }
+    if (isNumeric) {
+      password = password + numeric[Math.floor(Math.random() * numeric.length)];
+    }
+    if (isSpecialCharacter) {
+      password =
+        password + specialCharacter[Math.floor(Math.random() * numeric.length)];
+    }
+
+    var remaining = passwordlength - password.length ;
+    for (var i = 0; i < remaining; i++) {
+      if (isLowerCase) {
+        password =
+          password + lowerCase[Math.floor(Math.random() * lowerCase.length)];
+      }
+      if (isUpperCase) {
+        password =
+          password + upperCase[Math.floor(Math.random() * upperCase.length)];
+      }
+      if (isNumeric) {
+        password =
+          password + numeric[Math.floor(Math.random() * numeric.length)];
+      }
+      if (isSpecialCharacter) {
+        password =
+          password +
+          specialCharacter[Math.floor(Math.random() * numeric.length)];
+      }
+    }
+  } else {
+    alert("invalid password length");
+  }
+  return password.substring(0,passwordlength)
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
 //document.querySelector("#generate").addEventListener("click", writePassword);
